c9acb58b3fc2560d44c327c0d439a670bd5a2826
Bastien Teissier <bastient@theodo.fr>
2026-03-24 11:15:30 +0100

    feat: add chat request/response contracts

--- packages/contracts/src/http/index.ts
+++ packages/contracts/src/http/index.ts
@@ -4,3 +4,9 @@ export {
   type MovieSearchRequest,
   type MovieSearchResponse,
 } from "./movie-search.js";
+export {
+  chatRequestSchema,
+  chatResponseSchema,
+  type ChatRequest,
+  type ChatResponse,
+} from "./chat.js";
