05a998b7d10ee8161115d8c550f605bc3c571c11
Bastien Teissier <bastient@theodo.fr>
2026-03-29 15:26:28 +0200

    remove dead chat contracts replaced by AG-UI

--- packages/contracts/src/http/index.ts
+++ packages/contracts/src/http/index.ts
@@ -4,9 +4,3 @@ export {
   type MovieSearchRequest,
   type MovieSearchResponse,
 } from "./movie-search.js";
-export {
-  chatRequestSchema,
-  chatResponseSchema,
-  type ChatRequest,
-  type ChatResponse,
-} from "./chat.js";
