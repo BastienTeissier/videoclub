aeb69e8405213097bcc249b8e6e63a85d270b23e
Bastien Teissier <bastient@theodo.fr>
2026-04-01 09:26:27 +0200

    feat(contracts): add watchlist HTTP contracts

--- packages/contracts/src/http/index.ts
+++ packages/contracts/src/http/index.ts
@@ -4,3 +4,11 @@ export {
   type MovieSearchRequest,
   type MovieSearchResponse,
 } from "./movie-search.js";
+export {
+  addToWatchlistResponseSchema,
+  removeFromWatchlistResponseSchema,
+  watchlistResponseSchema,
+  type AddToWatchlistResponse,
+  type RemoveFromWatchlistResponse,
+  type WatchlistResponse,
+} from "./watchlist.js";
